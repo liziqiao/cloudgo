a6932e6cf937dc0144c6463b755e844c881cea9c
liziqiao <1240148677@qq.com>
2017-11-15 00:31:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 )
 
 const (
+  //设置默认端口为8080
   PORT string = "8080"
 )
 
 func main() {
-  port := os.Getenv("PORT")
+  port := os.Getenv("PORT")//获取去环境变量，看是否存在PORT
   if len(port) == 0 {
       port = PORT
   }
+  //解析命令行
   pPort := flag.StringP("port", "p", PORT, "listening PORT")
   flag.Parse()
   if len(*pPort) != 0 {
       port = *pPort
   }
+  //运行服务端
   server.NewServer(port)
 }
